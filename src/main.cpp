819bc50f947649c57b440bc789f0b3120734a488
Zeki <nguyenhatrung411@gmail.com>
2024-10-14 18:23:49 +0900

    Update settings.json and platformio.ini

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,34 @@
 #include <deca_device_api.h>
 #include <SPI.h>
 
+#include "deca_probe_interface.h"
+#include "deca_spi.h"
+#include "port.h"
 
 void setup() {
   // put your setup code here, to run once:
+
   Serial.begin(115200);
-  Serial.println("Hello World");
-  deca_sleep(1000);
-  Serial.println("Goodbye World");
+  while (!Serial);
+
+  SPI.begin();
+  pinMode(DW_CS_PIN, OUTPUT);
+  digitalWrite(DW_CS_PIN, HIGH);
+
+  // Initialize other DW3000 pins
+  // pinMode(DW_IRQ_PIN, INPUT);
+  // pinMode(DW_RST_PIN, OUTPUT);
+  // digitalWrite(DW_RST_PIN, HIGH); // Assuming HIGH is inactive
+  // pinMode(DW_WAKEUP_PIN, OUTPUT);
+  // digitalWrite(DW_WAKEUP_PIN, HIGH); // Assuming HIGH is inactive
+
+  // Initialize DW3000
+  if (dwt_probe((struct dwt_probe_s *)&dw3000_probe_interf) != DWT_SUCCESS) {
+    Serial.println("Failed to probe DW3000");
+    while (1);
+  }
+
+  
 }
 
 void loop() {
