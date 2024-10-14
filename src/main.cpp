1a1928ac20b3279ff95f09f20a0ed4409c24ab00
Zeki <nguyenhatrung411@gmail.com>
2024-10-14 15:48:18 +0900

    Add Dw3000 submodule and update main.cpp
    This commit adds the Dw3000 submodule and updates the main.cpp file. The Dw3000 submodule is added as a dependency to the project. In the main.cpp file, the code is modified to include the necessary headers for the Dw3000 module and to initialize the serial communication. The "Hello World" and "Goodbye World" messages are printed to the serial monitor with a delay of 1 second in between.
    
    Update .vscode settings
    
    This commit adds a settings.json file to the .vscode directory. The settings.json file includes file associations for specific header files, such as port.h, assert.h, and spi.h, to be recognized as C files.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,14 @@
 #include <Arduino.h>
+#include <deca_device_api.h>
+#include <SPI.h>
 
 
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(115200);
+  Serial.println("Hello World");
+  deca_sleep(1000);
+  Serial.println("Goodbye World");
 }
 
 void loop() {
