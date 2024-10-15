cb28ecf42ad1bc57fc087f4e569776d0da727898
Zeki <nguyenhatrung411@gmail.com>
2024-10-16 03:42:24 +0900

    Update Dw3000 submodule commit to use new commit hash and add necessary code

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,56 @@
 #include <Arduino.h>
-#include <deca_device_api.h>
 #include <SPI.h>
 
+#include "deca_regs.h" 
+#include "deca_device_api.h"
 #include "deca_probe_interface.h"
-#include "deca_spi.h"
 #include "port.h"
 
-void setup() {
-  // put your setup code here, to run once:
+/* Default communication configuration. We use default non-STS DW mode. */
+static dwt_config_t config = {
+    5,               /* Channel number. */
+    DWT_PLEN_128,    /* Preamble length. Used in TX only. */
+    DWT_PAC8,        /* Preamble acquisition chunk size. Used in RX only. */
+    9,               /* TX preamble code. Used in TX only. */
+    9,               /* RX preamble code. Used in RX only. */
+    1,               /* 0 to use standard 8 symbol SFD, 1 to use non-standard 8 symbol, 2 for non-standard 16 symbol SFD and 3 for 4z 8 symbol SDF type */
+    DWT_BR_6M8,      /* Data rate. */
+    DWT_PHRMODE_STD, /* PHY header mode. */
+    DWT_PHRRATE_STD, /* PHY header rate. */
+    (129 + 8 - 8),   /* SFD timeout (preamble length + 1 + SFD length - PAC size). Used in RX only. */
+    DWT_STS_MODE_OFF,
+    DWT_STS_LEN_64, /* STS length, see allowed values in Enum dwt_sts_lengths_e */
+    DWT_PDOA_M0     /* PDOA mode off */
+};
 
+/* The frame sent in this example is an 802.15.4e standard blink. It is a 12-byte frame composed of the following fields:
+ *     - byte 0: frame type (0xC5 for a blink).
+ *     - byte 1: sequence number, incremented for each new frame.
+ *     - byte 2 -> 9: device ID, see NOTE 1 below.
+ */
+static uint8_t tx_msg[] = {0xC5, 0, 'D', 'E', 'C', 'A', 'W', 'A', 'V', 'E'};
+/* Index to access to sequence number of the blink frame in the tx_msg array. */
+#define BLINK_FRAME_SN_IDX 1
+
+#define FRAME_LENGTH (sizeof(tx_msg) + FCS_LEN) // The real length that is going to be transmitted
+
+/* Inter-frame delay period, in milliseconds. */
+#define TX_DELAY_MS 500
+
+/* Values for the PG_DELAY and TX_POWER registers reflect the bandwidth and power of the spectrum at the current
+ * temperature. These values can be calibrated prior to taking reference measurements. */
+extern dwt_txconfig_t txconfig_options;
+
+bool tx_done = false;
+
+void tx_done_cb(const dwt_cb_data_t *cb_data)
+{
+  tx_done = true;
+}
+
+void setup() {
+
+  /* Platform Harware Configuration */
   Serial.begin(115200);
   while (!Serial);
 
@@ -16,10 +58,11 @@ void setup() {
   pinMode(DW_CS_PIN, OUTPUT);
   digitalWrite(DW_CS_PIN, HIGH);
 
-  // Initialize other DW3000 pins
-  pinMode(DW_IRQ_PIN, INPUT);
-  pinMode(DW_RST_PIN, INPUT); // Reset pin is not used
-  // pinMode(DW_WAKEUP_PIN, OUTPUT);
+  pinMode(DW_RST_PIN, INPUT); // Set reset pin as INPUT
+
+  pinMode(DW_IRQ_PIN, INPUT_PULLDOWN); // or call init_dw3000_irq();
+
+  /* DW3000 Configuration */
 
   reset_DW3000();
 
@@ -31,19 +74,121 @@ void setup() {
 
   delay(100);
 
-  uint32_t dev_id = dwt_readdevid();
-  Serial.print("Device ID: 0x");
-  Serial.println(dev_id, HEX);
-  
+  while (!dwt_checkidlerc()) // Need to make sure DW IC is in IDLE_RC before proceeding
+  {
+    Serial.println("DW IC is not in IDLE_RC state");
+    while (100);
+  }
 
-  // intialize DW3000
-  // if (dwt_initialise(DWT_READ_OTP_PID | DWT_READ_OTP_LID | DWT_READ_OTP_BAT | DWT_READ_OTP_TMP) != DWT_SUCCESS) {
-  //   Serial.println("Failed to initialize DW3000");
-  //   while (1);
-  // }
+  dwt_softreset();
+  delay(200);
+
+  while (!dwt_checkidlerc()) // Need to make sure DW IC is in IDLE_RC before proceeding
+  {
+    Serial.println("DW IC is not in IDLE_RC state");
+    while (100);
+  }
+
+  Serial.println("IDLE OK");
+
+  if (dwt_initialise(DWT_DW_INIT) == DWT_ERROR)
+  {
+    Serial.println("INIT FAILED");
+    while (100);
+  }
+  
+  Serial.println("INIT OK");
+
+  // Enabling LEDs here for debug so that for each TX the D1 LED will flash on DW3000 red eval-shield boards.
+  dwt_setleds(DWT_LEDS_ENABLE | DWT_LEDS_INIT_BLINK);
+
+  // Configure DW IC. See NOTE 5 below.
+  if (dwt_configure(&config)) // if the dwt_configure returns DWT_ERROR either the PLL or RX calibration has failed the host should reset the device
+  {
+    Serial.println("CONFIG FAILED");
+    while (100);
+  }
+
+  Serial.println("CONFIG OK");
+
+  /* Configure the TX spectrum parameters (power PG delay and PG Count) */
+  dwt_configuretxrf(&txconfig_options);
+
+  // Set event callbacks
+  dwt_setcallbacks(
+    tx_done_cb, 
+    NULL, 
+    NULL, 
+    NULL,
+    NULL,
+    NULL
+  );
+
+  // set IRQ event
+  dwt_setinterrupt(
+    (SYS_STATUS_ALL_TX | SYS_STATUS_ALL_RX_ERR | SYS_STATUS_ALL_RX_GOOD), 
+    0, 
+    DWT_ENABLE_INT_ONLY
+  );
+
+  /* NOTE: Enabling Interrupts should be done after the dwt_setcallbacks() and dwt_setinterrupt() calls.
+   * Otherwise, the DW IC may trigger unknown interrupt before the application is ready to handle it.
+   * It can cause the crash of the application.
+   */
+  enable_dw3000_irq();
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  /* Write frame data to DW IC and prepare transmission. See NOTE 3 below.*/
+  dwt_writetxdata(FRAME_LENGTH - FCS_LEN, tx_msg, 0); /* Zero offset in TX buffer. */
+
+  /* In this example since the length of the transmitted frame does not change,
+   * nor the other parameters of the dwt_writetxfctrl function, the
+   * dwt_writetxfctrl call could be outside the main while(1) loop.
+   */
+  dwt_writetxfctrl(FRAME_LENGTH, 0, 0); /* Zero offset in TX buffer, no ranging. */
+
+  /* Start transmission. */
+  dwt_starttx(DWT_START_TX_IMMEDIATE);
+  delay(10); // Sleep(TX_DELAY_MS);
+
+  /* Poll tx_done flag until the DW IC signals that the frame has been sent. 
+   * tx_done is a global flag set in the tx_done_cb callback function.
+   */
+  while (!tx_done) {
+    Serial.println("WHAT!!!\r\n");
+    delay(1);
+  }
+
+  tx_done = false; // Clear the flag
+
+  /* Clear TX frame sent event. */
+  dwt_write32bitreg(SYS_STATUS_ID, SYS_STATUS_TXFRS_BIT_MASK);
+
+  Serial.println("TX Frame Sent");
+
+  /* Execute a delay between transmissions. */
+  delay(TX_DELAY_MS);
+
+  /* Increment the blink frame sequence number (modulo 256). */
+  tx_msg[BLINK_FRAME_SN_IDX]++;
 }
 
+/*****************************************************************************************************************************************************
+ * NOTES:
+ *
+ * 1. The device ID is a hard coded constant in the blink to keep the example simple but for a real product every device should have a unique ID.
+ *    For development purposes it is possible to generate a DW IC unique ID by combining the Lot ID & Part Number values programmed into the
+ *    DW IC during its manufacture. However there is no guarantee this will not conflict with someone else�s implementation. We recommended that
+ *    customers buy a block of addresses from the IEEE Registration Authority for their production items. See "EUI" in the DW IC User Manual.
+ * 2. In a real application, for optimum performance within regulatory limits, it may be necessary to set TX pulse bandwidth and TX power, (using
+ *    the dwt_configuretxrf API call) to per device calibrated values saved in the target system or the DW IC OTP memory.
+ * 3. dwt_writetxdata() takes the full size of tx_msg as a parameter but only copies (size - 2) bytes as the check-sum at the end of the frame is
+ *    automatically appended by the DW IC. This means that our tx_msg could be two bytes shorter without losing any data (but the sizeof would not
+ *    work anymore then as we would still have to indicate the full length of the frame to dwt_writetxdata()).
+ * 4. We use polled mode of operation here to keep the example as simple as possible, but the TXFRS status event can be used to generate an interrupt.
+ *    Please refer to DW IC User Manual for more details on "interrupts".
+ * 5. Desired configuration by user may be different to the current programmed configuration. dwt_configure is called to set desired
+ *    configuration.
+ ****************************************************************************************************************************************************/
