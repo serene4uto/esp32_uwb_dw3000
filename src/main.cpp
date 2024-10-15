720803396ac7e68fdb8cb26a99d7d046263bdb8f
Zeki <nguyenhatrung411@gmail.com>
2024-10-15 16:29:53 +0900

    Update Dw3000 submodule commit to use new commit hash and remove unnecessary code

--- src/main.cpp
+++ src/main.cpp
@@ -19,23 +19,7 @@ void setup() {
   // Initialize other DW3000 pins
   pinMode(DW_IRQ_PIN, INPUT);
   pinMode(DW_RST_PIN, INPUT); // Reset pin is not used
-  // pinMode(DW_WAKEUP_PIN, OUTPUT);
 
-  // reset_DW3000();
-
-  // // Initialize DW3000
-  // if (dwt_probe((struct dwt_probe_s *)&dw3000_probe_interf) != DWT_SUCCESS) {
-  //   Serial.println("Failed to probe DW3000");
-  //   while (1);
-  // }
-
-  // delay(100);
-
-  // // intialize DW3000
-  // if (dwt_initialise(DWT_READ_OTP_PID | DWT_READ_OTP_LID | DWT_READ_OTP_BAT | DWT_READ_OTP_TMP) != DWT_SUCCESS) {
-  //   Serial.println("Failed to initialize DW3000");
-  //   while (1);
-  // }
 }
 
 void loop() {
